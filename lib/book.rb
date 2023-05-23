ee9cee64d02decedba612b52b76de5a7758981d6
Brian <mahuibrian14@gmail.com>
2023-05-23 12:18:44 +0300

    Done

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,27 @@
 
 
+class Book
+
+    attr_accessor :initialize, :author_name, :genre, :turn_page
+
+    def initialize(title) 
+        @title="And Then There Were None"
+    end
+    
+    def author=(author_name)
+        @author_name="Agatha Christie"
+    end
+    
+    def page_count=(page_count)
+        @page_count=272
+    end
+    
+    def genre=(genre)
+        @genre=genre
+    end
+   
+    def turn_page=(turn_page)
+        puts "Flipping the page...wow, you read fast!"
+    end
+
+end
--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,28 @@
 # Make your shoe class here!
+class Shoe
+    attr_accessor :initialize, :color, :size, :material, :condition, :cobble
+    def initialize(brand)
+        @brand="Nike"
+    end
+
+    def color=(color)
+        @color=color
+    end
+
+    def size=(size)
+        @size=size
+    end
+
+    def material=(material)
+        @material=material
+    end
+    
+    def condition=(condition)
+        @condition="new"
+    end
+
+    def cobble=(cobble)
+        puts "Your shoe is good as new!"
+        
+    end
+end
